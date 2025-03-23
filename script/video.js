279a65ad44b166ced51d31ba4c035c693096862c
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-23 14:25:24 +0600

    load categories with api and show it in html or ui

---
+++ script/video.js
@@ -0,0 +1,40 @@
+// console.log('file connect');
+
+
+// 1. Fetch, Load and Show Categories on html
+
+// create loadCategories
+const loadCategories = () => {
+    // fetch the data
+    const data = fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        .then(res => res.json())
+        .then(data => DisplayCategories(data?.categories))
+        .catch(err => console.log(err))
+};
+
+
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
+
+// create DisplayCategories
+const DisplayCategories = (categories) => {
+    // add data in html
+    // console.log(categories); // see, data recevied or not
+
+    const categoryContainer = document.getElementById('categories');
+    categories.forEach(item => {
+        console.log(item);
+        // create a button element
+        const button = document.createElement('button');
+        button.classList = 'btn px-10 py-2 border-none bg-black text-white';
+        button.innerText = item?.category;
+
+        // add button to category content
+        categoryContainer.append(button);
+    });
+};
+
+
+loadCategories();
